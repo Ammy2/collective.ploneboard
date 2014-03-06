5c25a3d91d20a574f609413e37e765de4f79ee77
Ammy2 <aman2arora2@gmail.com>
2014-03-06 22:01:26 +0530

    New usecase (#2)

--- src/collective/ploneboard/interfaces.py
+++ src/collective/ploneboard/interfaces.py
@@ -1,14 +1,17 @@
 # -*- coding: UTF-8 -*-
 from plone.app.textfield import RichText
 from plone.directives import form
-
+from zope import schema
 from collective.ploneboard import _
 
 
 class IMessageboard(form.Schema):
     """
     """
-
+    category = schema.Text(
+		    title=_(u"Categories"),
+		    required=True
+		    )
 
 class ITopic(form.Schema):
     """
